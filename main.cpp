d9deef1c7c839681b25f5f4ac42a4626cc8ba49b
omer-io <omer.jamil@rakurai.io>
2024-07-09 15:31:38 +0500

    gender feature added

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main() {
-    string your_name;
+    string your_name, your_gender;
     cout << "Enter your name: ";
     cin >> your_name;
     cout << "\nYour name is: " << your_name;
+    cout << "Enter your gender: ";
+    cin >> your_gender;
+    cout << "\nYour gender is: " << your_gender;
 }
