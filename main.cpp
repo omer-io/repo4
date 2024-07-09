a0b94e956386a23003099705add4f59a620a0e50
omer-io <omer.jamil@rakurai.io>
2024-07-09 15:27:32 +0500

    Added new feature

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main() {
-    string your_name;
+    string your_name, your_age;
     cout << "Enter your name: ";
     cin >> your_name;
-    cout << "\nYour name is: " << your_name;
+    cout << "\nHello, " << your_name;
+    cout << "Enter your age: ";
+    cin >> your_age;
+    cout << "\nYour age is " << your_age;
 }
