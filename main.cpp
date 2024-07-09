52884e4a21895e8507fceafb34380e0a938e14e6
omer-io <omer.jamil@rakurai.io>
2024-07-09 15:17:15 +0500

    main.cpp added/

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string your_name;
+    cout << "Enter your name: ";
+    cin >> your_name;
+    cout << "\nYour name is: " << your_name;
+}
