71e4f9173af10f5a72385719e4f49824c91d14df
omer-io <omer.jamil@rakurai.io>
2024-07-10 09:52:27 +0500

    added school feature

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    string your_name, your_age, your_gender;
+    string your_name, your_age, your_school;
     cout << "Enter your name: ";
     cin >> your_name;
     cout << "\nYour name is: " << your_name;
